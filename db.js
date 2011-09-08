dee11ccb45365da62d8bdc2f2d195e6439eaeda5
Nate Long <long.nathaniel@gmail.com>
2011-09-07 20:40:29 -0700

    Added main app files (no dependencies yet)

---
+++ db.js
@@ -0,0 +1,37 @@
+var fs = require( 'fs' );
+var cradle = require( 'cradle' );
+var db = new( cradle.Connection )( 'odyssey.natelong.net' ).database( 'foosball' );
+
+var getGUID = function getGUID() {
+    var S4 = function() {
+       return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+    };
+    return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+}
+
+var write = function write( dataObject ){
+	db.save( getGUID(), dataObject, function( err, res ){
+		if( err ){
+			console.log( '[ERROR] Couldn\'t add record: %o', dataObject );
+		}else{
+			console.log( '[INFO] Successfully added record: %o', dataObject );
+		}
+	});
+};
+
+var get = function get( onComplete ){
+	db.view( 'default/all', function( err, data ){
+		onComplete( err, data );
+	});
+};
+
+var getSinceDate = function getSinceDate( date, onComplete ){
+	db.view( 'default/bydate', { 'querystring': 'startkey=' + date }, function( err, data ){
+		console.log( '[INFO] getSinceDate returned %s rows since %s', data.length, date.valueOf() );
+		onComplete( err, data );
+	});
+};
+
+exports.write = write;
+exports.get = get;
+exports.getSinceDate = getSinceDate;
---
+++ foos.js
@@ -0,0 +1,254 @@
+var http = require( 'http' );
+var db = require( './db.js' );
+console.log( '\n\nAttempting to start server... ' );
+
+var foos = {};
+foos.handlers = {};
+foos.helpers = {};
+
+foos.handlers.favicon = function( req, res ){
+	var result = {
+		headers: null,
+		responseCode: 204,
+		content: null
+	};
+	
+	res.writeHead( result.responseCode, result.headers );
+	res.end( result.content || null );
+};
+
+foos.handlers.generic = function( req, res ){
+	var result = {
+		headers: {
+			'Content-type': 'text/html'
+		},
+		responseCode: 200,
+		content: '<!DOCTYPE html><html><head><title>AWS Node Test</title></head><body><h1>Hello from NodeJS!</h1></body></html>'
+	};
+	
+	res.writeHead( result.responseCode, result.headers );
+	res.end( result.content || null );
+};
+
+foos.handlers.addGame = function( req, res ){
+	var rows;
+	var requestString = '';
+	req.on( 'data', function( chunk ){
+		requestString += chunk;
+	});
+	req.on( 'end', function(){
+		var status;
+		var gameObject;
+		var i;
+		var birdCatcher;
+		
+		try{
+			var reqObj = JSON.parse( requestString );
+		}catch( e ){
+			console.log( '[ERROR] Error parsing request information: %s', e );
+			console.log( requestString );
+			res.writeHead( 500 );
+			res.end();
+			return;
+		}
+		if( !reqObj.winner || !reqObj.winScore || !reqObj.loser || !reqObj.loseScore ){
+			console.log( '[ERROR] Request object didn\'t have all required parameters: %s', reqObj );
+			res.writeHead( 500 );
+			res.end();
+			return;
+		}
+		
+		gameObject = {
+			winner: reqObj.winner,
+			winScore: parseInt( reqObj.winScore ),
+			loser: reqObj.loser,
+			loseScore: parseInt( reqObj.loseScore ),
+			dateAdded: Date.now()
+		};
+		if( reqObj.winner.toLowerCase().indexOf( 'bird' ) === -1 && reqObj.winner.toLowerCase().indexOf( 'flyer' ) === -1 ){
+			status = db.write( gameObject );
+			foos.helpers.getCurrentRows( function( err, data ){
+				rows = JSON.stringify({ rows: data });
+				res.writeHead( status, {
+					'content-type': 'application/json',
+					'content-length': rows.length
+				});
+				res.end( rows );
+			});
+		}else{
+			console.log( '[INFO] BirdCatcher engaged!' );
+			status = 200;
+			foos.helpers.getCurrentRows( function( err, data ){
+				birdCatcher = data;
+				birdCatcher.push({
+					name: reqObj.winner,
+					wins: 1,
+					losses: 0,
+					for: parseInt( reqObj.winScore ),
+					against: parseInt( reqObj.loseScore )
+				});
+				rows = JSON.stringify({ rows: birdCatcher });
+				res.writeHead( status, {
+					'content-type': 'application/json',
+					'content-length': rows.length
+				});
+				res.end( rows );
+			});
+		}
+	});
+};
+
+foos.handlers.all = function all( req, res ){
+	foos.helpers.getCurrentRows(function( err, data ){
+		var rows = JSON.stringify({ rows: data });
+		res.writeHead( 200, {
+			'content-type': 'application/json',
+			'content-length': rows.length
+		});
+		res.end( rows );
+	});
+};
+
+foos.handlers.lastWeek = function lastWeek( req, res ){
+	foos.helpers.getCurrentRows(function( err, data ){
+		var rows = JSON.stringify({ rows: data });
+		res.writeHead( 200, {
+			'content-type': 'application/json',
+			'content-length': rows.length
+		});
+		res.end( rows );
+	}, new Date( Date.now() - (1000 * 60 * 60 * 24 * 7) ) );
+};
+
+foos.helpers.getCurrentRows = function getCurrentRows( onComplete, date ){
+	var games;
+	var players = {};
+	var rows = [];
+	
+	var sortFunc = function sortFunc( a, b ){
+		if( a.wins < b.wins ){
+			return 1;
+		}else if( a.wins > b.wins ){
+			return -1;
+		}else{
+			return 0;
+		}
+	};
+
+	var doSort = function doSort( err, games ){
+		var i = games.length;
+		var tmpGame;
+
+		while( i-- ){
+			tmpGame = games[ i ].doc || games[ i ].value;
+
+			if( !players[ tmpGame.winner ] ){
+				players[ tmpGame.winner ] = {
+					name: tmpGame.winner,
+					wins: 1,
+					losses: 0,
+					for: tmpGame.winScore,
+					against: tmpGame.loseScore
+				};
+			}else{
+				players[ tmpGame.winner ].wins += 1;
+				players[ tmpGame.winner ].for += tmpGame.winScore;
+				players[ tmpGame.winner ].against += tmpGame.loseScore;
+			}
+		
+			if( !players[ tmpGame.loser ] ){
+				players[ tmpGame.loser ] = {
+					name: tmpGame.loser,
+					wins: 0,
+					losses: 1,
+					for: tmpGame.loseScore,
+					against: tmpGame.winScore
+				}
+			}else{
+				players[ tmpGame.loser ].losses += 1;
+				players[ tmpGame.loser ].for += tmpGame.loseScore;
+				players[ tmpGame.loser ].against += tmpGame.winScore;
+			}
+		}
+
+		for( i in players ) if ( players.hasOwnProperty( i ) ){
+			rows.push( players[ i ] );
+		}
+
+		onComplete( null, rows.sort( sortFunc ) );
+	};
+	
+	if( date ){
+		db.getSinceDate( date, doSort );
+	}else{
+		db.get( doSort );
+	}
+};
+
+foos.routes = [
+	{
+		name: 'Get Current Rows',
+		search: /\/all/,
+		func: foos.handlers.all,
+		methods: [ 'GET', 'POST' ]
+	},{
+		name: 'Add Game Record',
+		search: /\/add/,
+		func: foos.handlers.addGame,
+		methods: [ 'POST' ]
+	},{
+		name: 'Get Rows Since Last Week',
+		search: /\/lastweek/,
+		func: foos.handlers.lastWeek,
+		methods: [ 'GET', 'POST' ]
+	},{
+		name: 'Favicon',
+		search: /favicon.ico/,
+		func: foos.handlers.favicon,
+		methods: [ 'GET' ]
+	},{
+		name: 'Generic Test',
+		search: /.+/,
+		func: foos.handlers.generic,
+		methods: [ 'GET' ]
+	}
+];
+
+foos.init = function init(){
+	var srv = http.createServer( foos.routeRequest );
+
+	srv.listen( 8080, function(){
+		console.log( 'Server successfully started.' );
+	});
+};
+
+foos.routeRequest = function( req, res ){
+	var i, len;
+	var route, result;
+	var routeFound = false;
+
+	for( i = 0, len = foos.routes.length; i < len; i++ ){
+		if( foos.routes[ i ].search.test( req.url ) && foos.routes[ i ].methods.indexOf( req.method ) !== -1 ){
+			route = foos.routes[ i ];
+			console.log( '[ROUTE] Executing route: %s', route.name );
+			routeFound = true;
+
+			try{
+				route.func( req, res );
+			}catch( e ){
+				console.error( '[ERROR] Uncaught error while handling request: %s', e );
+				res.writeHead( 500, { 'Content-Type': 'text/plain' } );
+				res.end( 'Error 500' );
+			}
+			
+			return true;
+		}
+	}
+	if( !routeFound ){
+		console.error( '[ERROR] No route found for url %s', req.url );
+		res.writeHead( 404, { 'Content-Type': 'text/plain' } );
+		res.end( '404\'d' );
+	}
+};
+
+foos.init();
