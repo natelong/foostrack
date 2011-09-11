dcb32be6f8688f002cc61a8123de7346f90ace32
Nate Long <long.nathaniel@gmail.com>
2011-09-10 20:22:40 -0700

    Made a bunch of changes to the helpers file so that the continuations actually work

--- foos.js
+++ foos.js
@@ -1,10 +1,11 @@
 var http = require( 'http' );
 var db = require( './db.js' );
+var helpers = require( './helpers.js' );
 console.log( '\n\nAttempting to start server... ' );
 
 var foos = {};
 foos.handlers = {};
-foos.helpers = {};
+foos.helpers = helpers;
 
 foos.handlers.favicon = function( req, res ){
 	var result = {
@@ -123,71 +124,6 @@ foos.handlers.getGameList = function getGameList( req, res ){
 	});
 };
 
-foos.helpers.getCurrentRows = function getCurrentRows( onComplete, date ){
-	var games;
-	var players = {};
-	var rows = [];
-	
-	var sortFunc = function sortFunc( a, b ){
-		if( a.wins < b.wins ){
-			return 1;
-		}else if( a.wins > b.wins ){
-			return -1;
-		}else{
-			return 0;
-		}
-	};
-
-	var doSort = function doSort( err, games ){
-		var i = games.length;
-		var tmpGame;
-
-		while( i-- ){
-			tmpGame = games[ i ].doc || games[ i ].value;
-
-			if( !players[ tmpGame.winner ] ){
-				players[ tmpGame.winner ] = {
-					name: tmpGame.winner,
-					wins: 1,
-					losses: 0,
-					for: tmpGame.winScore,
-					against: tmpGame.loseScore
-				};
-			}else{
-				players[ tmpGame.winner ].wins += 1;
-				players[ tmpGame.winner ].for += tmpGame.winScore;
-				players[ tmpGame.winner ].against += tmpGame.loseScore;
-			}
-		
-			if( !players[ tmpGame.loser ] ){
-				players[ tmpGame.loser ] = {
-					name: tmpGame.loser,
-					wins: 0,
-					losses: 1,
-					for: tmpGame.loseScore,
-					against: tmpGame.winScore
-				}
-			}else{
-				players[ tmpGame.loser ].losses += 1;
-				players[ tmpGame.loser ].for += tmpGame.loseScore;
-				players[ tmpGame.loser ].against += tmpGame.winScore;
-			}
-		}
-
-		for( i in players ) if ( players.hasOwnProperty( i ) ){
-			rows.push( players[ i ] );
-		}
-
-		onComplete( null, rows.sort( sortFunc ) );
-	};
-	
-	if( date ){
-		db.getSinceDate( date, doSort );
-	}else{
-		db.get( doSort );
-	}
-};
-
 foos.routes = [
 	{
 		name: 'Get Current Rows',
@@ -225,7 +161,7 @@ foos.routes = [
 foos.init = function init(){
 	var srv = http.createServer( foos.routeRequest );
 
-	srv.listen( 8081, function(){
+	srv.listen( 8080, function(){
 		console.log( 'Server successfully started.' );
 	});
 };
