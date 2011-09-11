3bf2f6ec8dfc85d9580afdfd29a323c878c8d6e8
Nate Long <long.nathaniel@gmail.com>
2011-09-10 20:23:08 -0700

    Made a bunch of changes to the helpers file so that the continuations actually work

---
+++ helpers.js
@@ -0,0 +1,70 @@
+var db = require( './db.js' );
+
+var sortFunc = function sortFunc( a, b ){
+	if( a.wins < b.wins ){
+		return 1;
+	}else if( a.wins > b.wins ){
+		return -1;
+	}else{
+		return 0;
+	}
+};
+
+var doSort = function doSort( err, games, onComplete ){	
+	var players = {};
+	var rows = [];
+	var i = games.length;
+	var tmpGame;
+
+	while( i-- ){
+		tmpGame = games[ i ].doc || games[ i ].value;
+
+		if( !players[ tmpGame.winner ] ){
+			players[ tmpGame.winner ] = {
+				name: tmpGame.winner,
+				wins: 1,
+				losses: 0,
+				for: tmpGame.winScore,
+				against: tmpGame.loseScore
+			};
+		}else{
+			players[ tmpGame.winner ].wins += 1;
+			players[ tmpGame.winner ].for += tmpGame.winScore;
+			players[ tmpGame.winner ].against += tmpGame.loseScore;
+		}
+	
+		if( !players[ tmpGame.loser ] ){
+			players[ tmpGame.loser ] = {
+				name: tmpGame.loser,
+				wins: 0,
+				losses: 1,
+				for: tmpGame.loseScore,
+				against: tmpGame.winScore
+			}
+		}else{
+			players[ tmpGame.loser ].losses += 1;
+			players[ tmpGame.loser ].for += tmpGame.loseScore;
+			players[ tmpGame.loser ].against += tmpGame.winScore;
+		}
+	}
+
+	for( i in players ) if ( players.hasOwnProperty( i ) ){
+		rows.push( players[ i ] );
+	}
+
+	onComplete( null, rows.sort( sortFunc ) );
+};
+
+var getCurrentRows = function getCurrentRows( onComplete, date ){
+	var complete = function( err, games ){
+		doSort( err, games, onComplete );
+	}
+
+	if( date ){
+		db.getSinceDate( date, complete );
+	}else{
+		db.get( complete );
+	}
+};
+
+exports.getCurrentRows = getCurrentRows;
