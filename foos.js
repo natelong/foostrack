707e1328245dcd5498881695295e77d5ca96a413
Nate Long <long.nathaniel@gmail.com>
2011-09-11 11:00:24 -0700

    moved initialization call up, since routes can be added at any time, and it's best to try to initialize the main server first

--- foos.js
+++ foos.js
@@ -2,11 +2,11 @@ var foos = require( './server.js' );
 
 console.log( '\n\nAttempting to start server... ' );
 
+foos.init();
+
 foos.addRoute( './handlers/all.js' );
 foos.addRoute( './handlers/addgame.js' );
 foos.addRoute( './handlers/lastweek.js' );
 foos.addRoute( './handlers/getgamelist.js' );
 foos.addRoute( './handlers/favicon.js' );
 foos.addRoute( './handlers/generic.js' );
-
-foos.init();
